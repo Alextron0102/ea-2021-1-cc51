991d6c2333dfd45512d8ced5d08ce4efbe1485c7
Alextron0102 <alextron0102@gmail.com>
2021-05-11 22:06:13 -0500

    normalizacion de valores NA

--- code/analisis_explotatorio_de_datos.R
+++ code/analisis_explotatorio_de_datos.R
@@ -19,3 +19,37 @@ sin_valor <- function(x){
   }
 }
 sin_valor(housing)
+#Tecnicas de normalización
+#Omision
+housing.limpio.omision <- housing[complete.cases(housing),]
+#Generacion de valores a partir del MAS:
+random.df <- function(df, cols){ 
+  nombres <- names(df) 
+  for (col in cols) {
+    df[nombres[col]] <- rand.valor(df[,col])
+  }
+  df
+}
+rand.valor <- function(x){ 
+  faltantes <- is.na(x)
+  tot.faltantes <- sum(faltantes)
+  x.obs <- x[!faltantes]
+  valorado <- x
+  valorado[faltantes] <- sample(x.obs, tot.faltantes, replace = TRUE)
+  return (valorado)
+}
+housing.limpio.mas <- random.df(housing, c(3,4,5,6,7,8,9,10,11,12,26))
+write.table(housing.limpio.mas, file = "../outputs/housinglimpiomas.txt", sep = "\t",row.names = TRUE, col.names = NA)
+sin_valor(housing.limpio.mas)
+#Generacion de valores con la media de la población
+media.poblacion <- function(df, cols){
+  nombres <- names(df)
+  for(col in cols){
+    df[nombres[col]] <- ifelse(is.na(df[,col]), mean(df[,col], na.rm = TRUE), df[,col])
+  }
+  df
+}
+housing.limpio.media <- media.poblacion(housing, c(3,4,6,7,8,9,10,11,12,26))
+write.table(housing.limpio.media, file = "../outputs/housinglimpiomedia.txt", sep = "\t",row.names = TRUE, col.names = NA)
+sin_valor(housing.limpio.media)
+
